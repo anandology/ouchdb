3c19e302ab51d28b5f1112c7dc5ce6ba180913a5
Anand Chitipothu <anandology@gmail.com>
2011-06-06 14:28:39 +0530

    Added long_description.

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from ouchdb.version import VERSION
 setup(
     name='ouchdb',
     version=VERSION,
+    
     description='OuchDB is an implementation of CouchDB API on relational databases',
+    long_description=open("Readme.rst").read(),
     packages=find_packages(exclude=["ez_setup"]),
     install_requires="web.py gunicorn".split(),
-
+    
     author="Anand Chitipothu",
     author_email="anandology@gmail.com",
-
     license="Apache License, Version 2.0",
     platforms=["any"],
 )
